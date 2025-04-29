3e19e11bb83645f9bac4602865db4595719c4dfd
profsam97 <proftoby4real@hotmail.com>
2025-04-29 18:31:13 +0100

    added cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,12 @@ import { ZodValidationPipe } from 'nestjs-zod';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ZodValidationPipe());
+  app.enableCors({
+    origin: [process.env.FRONTEND_STAGING_URL],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Accept', 'Authorization', 'Cookie'],
+  });
   await app.listen(3000);
 }
 bootstrap();
