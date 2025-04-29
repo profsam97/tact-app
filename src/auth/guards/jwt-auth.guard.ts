3c86984edfc32ea4d1c332163d21038f5ceb1efc
profsam97 <proftoby4real@hotmail.com>
2025-04-29 10:26:31 +0100

    added jwt guards

---
+++ src/auth/guards/jwt-auth.guard.ts
@@ -0,0 +1,26 @@
+import { Injectable, ExecutionContext } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { GqlExecutionContext } from '@nestjs/graphql';
+
+@Injectable()
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  getRequest(context: ExecutionContext) {
+    const ctx = GqlExecutionContext.create(context);
+    const request = ctx.getContext().req;
+    return request;
+  }
+
+  handleRequest(
+    err: any,
+    user: any,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    info: any,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    context: ExecutionContext,
+  ) {
+    if (err || !user) {
+      throw err || new Error('Unauthorized');
+    }
+    return user;
+  }
+}
