aea6de69a8614b1f1b832863085a87b4c4cf7cde
profsam97 <proftoby4real@hotmail.com>
2025-04-29 18:28:47 +0100

    added pagination fieild

--- src/common/graphql-types.ts
+++ src/common/graphql-types.ts
@@ -1,5 +1,25 @@
-import { Field, ObjectType } from '@nestjs/graphql';
+import { Field, ObjectType, Int, InputType } from '@nestjs/graphql';
+import { Type } from '@nestjs/common';
 
+// GraphQL Input Type for Pagination
+@InputType()
+export class PaginationInput {
+  @Field(() => Int, {
+    nullable: true,
+    description: 'Number of items to take',
+    defaultValue: 10,
+  })
+  take?: number;
+
+  @Field(() => Int, {
+    nullable: true,
+    description: 'Number of items to skip',
+    defaultValue: 0,
+  })
+  skip?: number;
+}
+
+//  GraphQL type for SubDepartment
 @ObjectType()
 export class SubDepartment {
   @Field(() => String)
@@ -11,3 +31,34 @@ export class SubDepartment {
   @Field(() => String)
   departmentId: string;
 }
+
+//  GraphQL type for Department
+@ObjectType()
+export class DepartmentType {
+  @Field(() => String)
+  id: string;
+
+  @Field()
+  name: string;
+
+  @Field(() => [SubDepartment], { nullable: 'itemsAndList' })
+  subDepartments?: SubDepartment[] | null;
+}
+
+// Generic factory function for creating Paginated Response types
+export function PaginatedResponse<TItem>(TItemClass: Type<TItem>) {
+  @ObjectType({ isAbstract: true })
+  abstract class PaginatedResponseClass {
+    @Field(() => [TItemClass])
+    items: TItem[];
+
+    @Field(() => Int)
+    totalCount: number;
+  }
+  return PaginatedResponseClass;
+}
+
+@ObjectType()
+export class PaginatedDepartmentResponse extends PaginatedResponse(
+  DepartmentType,
+) {}
