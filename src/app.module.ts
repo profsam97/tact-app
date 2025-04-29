7d7fa14d91d304378362c98f416f5bd670d0f70c
profsam97 <proftoby4real@hotmail.com>
2025-04-29 12:19:30 +0100

    updated app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,33 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { DepartmentsModule } from './departments/departments.module';
+import { SubDepartmentsModule } from './sub-departments/sub-departments.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      // Configure GraphQLModule
+      driver: ApolloDriver,
+      autoSchemaFile: 'schema.gql',
+      sortSchema: true,
+      context: ({ req }) => ({ req }),
+    }),
+    PrismaModule,
+    UserModule,
+    AuthModule,
+    DepartmentsModule,
+    SubDepartmentsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
